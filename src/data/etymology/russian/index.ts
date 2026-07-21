307a5c35545836dac6aa386a8b13c3d6b9238b63
Parthiv Rawat <parthiv05022000@gmail.com>
2026-07-21 18:37:06 +0530

    data(etymology): add etymology datasets for additional learning languages

--- src/data/etymology/russian/index.ts
+++ src/data/etymology/russian/index.ts
@@ -1,3 +1,20 @@
 import { Etymology } from '@/types';
+import greetingsData from './greetings.json';
+import numbersData from './numbers.json';
+import verbsData from './verbs.json';
+import dailyUseNounsData from './daily-use-nouns.json';
+import adjectivesData from './adjectives.json';
+import adverbsData from './adverbs.json';
+import prepositionsData from './prepositions.json';
+import functionWordsData from './function-words.json';
 
-export const etymologyData: Record<string, Etymology> = {};
+export const etymologyData: Record<string, Etymology> = {
+  ...greetingsData,
+  ...numbersData,
+  ...verbsData,
+  ...dailyUseNounsData,
+  ...adjectivesData,
+  ...adverbsData,
+  ...prepositionsData,
+  ...functionWordsData,
+};
